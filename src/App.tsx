af9c1a3f3d9868837345f6d995c7ae6dc90b90eb
Nigel Alford <nigel.alford@wahoofitness.com>
2022-02-09 19:32:57 -0500

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import routes from './pages/index'
+import routes from './pages/index';
 
 require('dotenv').config();
 
 // add the scrollTo fix for react-router
 
 const App = () => {
-
-    return (
-        <Routes>
-            {
-            routes.map((data,index) => (
-                <Route path={data.path} element={data.component} key={index} />
-            ))
-            }
-      </Routes>
-    );
-}
+  return (
+    <Routes>
+      {routes.map((data, index) => (
+        <Route path={data.path} element={data.component} key={index} />
+      ))}
+    </Routes>
+  );
+};
 
 export default App;
