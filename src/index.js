e1bf019571bb446cc69cb152904b07050e48dfe6
Nigel Alford <nigel.alford@wahoofitness.com>
2022-02-09 19:12:58 -0500

    level setting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import ScrollToTop from './ScrollToTop';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <BrowserRouter >
     <ScrollToTop />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
