499d6fcc80356119a32ec4eabe7340ad5526b0ee
Nigel Alford <nigel.alford@wahoofitness.com>
2022-02-10 14:01:45 -0500

    cleaning redundant index file and clearing .env from git tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import routes from './pages/index';
+import ExplorePage from './pages/Explore';
+import LiveAuctions from './pages/LiveAuctions';
+import ItemDetailsPage from './pages/ItemDetails';
+import ActivityPage from './pages/Activity';
+import BlogDetails from './pages/BlogDetails';
+import HelpCenter from './pages/HelpCenter';
+import AuthorsPage from './pages/Authors';
+import CreateItem from './pages/CreateItem';
+import EditProfile from './pages/EditProfile';
+import Ranking from './pages/Ranking';
+import NoResult from './pages/NoResult';
+import FAQ from './pages/FAQ';
+import ContactPage from './pages/Contact';
+import Home from './pages/Home';
 
 require('dotenv').config();
-
-// add the scrollTo fix for react-router
 
 const App = () => {
   return (
     <Routes>
-      {routes.map((data, index) => (
-        <Route path={data.path} element={data.component} key={index} />
-      ))}
+      <Route path="/" element={Home} />
+      <Route path="/explore" element={ExplorePage} />
+      <Route path="/live-auctions" element={LiveAuctions} />
+      <Route path="/item-details" element={ItemDetailsPage} />
+      <Route path="/activity" element={ActivityPage} />
+      <Route path="/blog-details" element={BlogDetails} />
+      <Route path="/help-center" element={HelpCenter} />
+      <Route path="/authors" element={AuthorsPage} />
+      <Route path="/create-item" element={CreateItem} />
+      <Route path="/edit-profile" element={EditProfile} />
+      <Route path="/ranking" element={Ranking} />
+      <Route path="/no-result" element={NoResult} />
+      <Route path="/faq" element={FAQ} />
+      <Route path="/contact" element={ContactPage} />
     </Routes>
   );
 };
