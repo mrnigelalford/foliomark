416a2d5ac8c008e8d572b959244e9998e171495b
Nigel Alford <nigel.alford@wahoofitness.com>
2022-02-12 08:52:16 -0500

    clening navbar

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,46 +1,5 @@
 const menus = [
   {
-    id: 1,
-    name: 'Home',
-    namesub: [
-      {
-        id: 1,
-        sub: 'Home 01',
-        links: '/',
-      },
-      {
-        id: 2,
-        sub: 'Home 02',
-        links: '/home-02',
-      },
-      {
-        id: 3,
-        sub: 'Home 03',
-        links: '/home-03',
-      },
-      {
-        id: 4,
-        sub: 'Home 04',
-        links: '/home-04',
-      },
-      {
-        id: 5,
-        sub: 'Home 05',
-        links: '/home-05',
-      },
-      {
-        id: 6,
-        sub: 'Home 06',
-        links: '/home-06',
-      },
-      {
-        id: 7,
-        sub: 'Home 07',
-        links: '/home-07',
-      },
-    ],
-  },
-  {
     id: 2,
     name: 'Explore',
     namesub: [
@@ -49,131 +8,30 @@ const menus = [
         sub: 'Explore 01',
         links: '/explore-01',
       },
-      {
-        id: 2,
-        sub: 'Explore 02',
-        links: '/explore-02',
-      },
-      {
-        id: 3,
-        sub: 'Explore 03',
-        links: '/explore-03',
-      },
-      {
-        id: 4,
-        sub: 'Explore 04',
-        links: '/explore-04',
-      },
-      {
-        id: 5,
-        sub: 'Live Auctions',
-        links: '/live-auctions',
-      },
-      {
-        id: 6,
-        sub: 'Item Details 01',
-        links: '/item-details-01',
-      },
-      {
-        id: 7,
-        sub: 'Item Details 02',
-        links: '/item-details-02',
-      },
     ],
   },
   {
     id: 3,
-    name: 'Activity',
+    name: 'Artists',
     namesub: [
       {
         id: 1,
-        sub: 'Activity 01',
-        links: '/activity-01',
-      },
-      {
-        id: 2,
-        sub: 'Activity 02',
-        links: '/activity-02',
+        sub: 'All Artists',
+        links: '/authors',
       },
     ],
   },
-  {
-    id: 4,
-    name: 'Community',
-    namesub: [
-      {
-        id: 1,
-        sub: 'Blog',
-        links: '/blog',
-      },
-      {
-        id: 2,
-        sub: 'Blog Details',
-        links: '/blog-details',
-      },
-      {
-        id: 3,
-        sub: 'Help Center',
-        links: '/help-center',
-      },
-    ],
-  },
-  {
-    id: 5,
-    name: 'Page',
-    namesub: [
-      {
-        id: 1,
-        sub: 'Authors 01',
-        links: '/authors-01',
-      },
-      {
-        id: 2,
-        sub: 'Authors 02',
-        links: '/authors-02',
-      },
-      {
-        id: 3,
-        sub: 'Wallet Connect',
-        links: '/wallet-connect',
-      },
-      {
-        id: 4,
-        sub: 'Create Item',
-        links: '/create-item',
-      },
-      {
-        id: 5,
-        sub: 'Edit Profile',
-        links: '/edit-profile',
-      },
-      {
-        id: 6,
-        sub: 'Ranking',
-        links: '/ranking',
-      },
-      {
-        id: 7,
-        sub: 'Login',
-        links: '/login',
-      },
-      {
-        id: 8,
-        sub: 'Sign Up',
-        links: '/sign-up',
-      },
-      {
-        id: 9,
-        sub: 'No Result',
-        links: '/no-result',
-      },
-      {
-        id: 10,
-        sub: 'FAQ',
-        links: '/faq',
-      },
-    ],
-  },
+  // {
+  //   id: 4,
+  //   name: 'Blog',
+  //   namesub: [
+  //     {
+  //       id: 2,
+  //       sub: 'Blog Details',
+  //       links: '/blog-details',
+  //     },
+  //   ],
+  // },
   {
     id: 7,
     name: 'Contact',
@@ -182,11 +40,6 @@ const menus = [
         id: 1,
         sub: 'Contact 1',
         links: '/contact-01',
-      },
-      {
-        id: 2,
-        sub: 'Contact 2',
-        links: '/contact-02',
       },
     ],
   },
